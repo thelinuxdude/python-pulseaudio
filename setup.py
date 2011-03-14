7c16269b129ae30b32d2feecebfbeea75a569f44
Valodim <valodim@mugenguild.com>
2011-03-14 01:37:47 +0100

    restructure as package, add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pulseaudio',
+      version='0.9.22',
+      description='PulseAudio Bindings',
+      author='Valodim',
+      author_email='valodim@mugenguild.com',
+      url='http://github.com/valodim/python-pulseaudio',
+      packages=['pulseaudio'],
+     )
