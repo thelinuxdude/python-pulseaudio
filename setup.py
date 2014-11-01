4f56267fb5e17da8e3f7da959df476c83b03ce45
Craig Matsuura <thelinuxdude4@gmail.com>
2014-10-31 19:31:54 -0600

    Updated for pa-4.0

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@
 from distutils.core import setup
 
 setup(name='libpulseaudio',
-      version='1.1',
+      version='4.0',
       description='simple libpulseaudio bindings',
       author='Valodim',
       author_email='valodim@mugenguild.com',
       license='LGPL',
-      url='http://github.com/valodim/python-pulseaudio',
+      url='http://github.com/thelinuxdude/python-pulseaudio',
       packages=['pulseaudio'],
-      provides=['libpulseaudio'],
-      download_url='http://datatomb.de/~valodim/libpulseaudio-1.1.tar.gz'
+      provides=['libpulseaudio']
      )
